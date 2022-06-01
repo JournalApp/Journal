0fe605823c8dad3c9c611f4a78f56b61b4da8b6a
Jarek Ceborski <jaroslaw.ceborski@gmail.com>
2022-06-01 08:50:06 +0200

    added login by typing refresh token

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import { theme } from 'themes'
 import { useUserContext } from 'context'
 import { Icon } from 'components'
+import { supabase } from 'utils'
 
 const Container = styled.div`
   position: fixed;
@@ -23,12 +24,72 @@ const ErrorMessage = styled.div`
   color: ${theme('color.error.main')};
 `
 
+const RTForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`
+
+const RTInput = styled.input`
+  font-size: 16px;
+  padding: 17px;
+  display: block;
+  margin: 4px;
+  border-radius: 100px;
+  border: 1px solid ${theme('color.primary.border')};
+  background-color: ${theme('color.primary.surface')};
+  color: ${theme('color.primary.main')};
+`
+
+const RTError = styled.div`
+  font-size: 16px;
+  padding: 17px;
+  display: block;
+  margin: 4px;
+  color: ${theme('color.error.main')};
+`
+
+const LoginWithToken = () => {
+  const [authError, setAuthError] = useState('')
+  const rt = useRef(null)
+
+  const keyPress = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      let refreshToken = rt.current.value
+      const { error } = await supabase.auth.signIn({ refreshToken })
+      if (error) {
+        console.log(error)
+        setAuthError(error.message)
+      }
+    } else {
+      setAuthError('')
+    }
+  }
+
+  return (
+    <RTForm onSubmit={(e) => e.preventDefault()}>
+      <RTInput
+        placeholder='Type refresh token and hit enter'
+        type='text'
+        name='refresh_token'
+        ref={rt}
+        size={50}
+        onKeyDown={(e) => keyPress(e)}
+      ></RTInput>
+      <RTError>{authError}</RTError>
+    </RTForm>
+  )
+}
+
 const Login = () => {
   const { authError } = useUserContext()
+
   return (
     <Container>
       <LoginButton href='https://www.journal.do/auth?action=signout'>Login in browser</LoginButton>
       <ErrorMessage>{authError}</ErrorMessage>
+      <LoginWithToken />
     </Container>
   )
 }
