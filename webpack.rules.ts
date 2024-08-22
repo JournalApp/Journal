527e6e60305f2d1ce45b53c9c7f0d6e855b8c704
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2024-08-22 14:17:55 +0200

    feat: change webpack configs to ts

--- webpack.rules.ts
+++ webpack.rules.ts
@@ -1,13 +1,15 @@
-module.exports = [
+import type { ModuleOptions } from 'webpack';
+
+export const rules: Required<ModuleOptions>['rules'] = [
   // Add support for native node modules
   {
     // We're specifying native_modules in the test because the asset relocator loader generates a
     // "fake" .node file which is really a cjs file.
-    test: /native_modules\/.+\.node$/,
+    test: /native_modules[/\\].+\.node$/,
     use: 'node-loader',
   },
   {
-    test: /\.(m?js|node)$/,
+    test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
     parser: { amd: false },
     use: {
       loader: '@vercel/webpack-asset-relocator-loader',
@@ -40,4 +42,4 @@ module.exports = [
       fullySpecified: false,
     },
   },
-]
+];
